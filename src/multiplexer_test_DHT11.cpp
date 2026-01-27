d2f7feb5614f08d99e6da97e199b1156591e5a59
marcio <marciogarcial412@gmail.com>
2026-01-27 14:14:34 -0500

    Created multiplexer with DHT11 Test File

--- src/multiplexer_test_DHT11.cpp
+++ src/multiplexer_test_DHT11.cpp
@@ -1,21 +1,27 @@
-#include <Arduino.h>
+#include <Arduino.h> //File to test INPUT/OUTPUT of Multiplexer
+#include <DHT.h>
 
 #define MUX_COM_PIN PA0
 #define SWITCH_PIN_A PA1
+#define DHTTYPE DHT11
+
+DHT dht(MUX_COM_PIN, DHTTYPE);
 
 void setup() {
-    pinMode(MUX_COM_PIN, INPUT_ANALOG);
     pinMode(SWITCH_PIN_A, OUTPUT);
+    dht.begin();
     Serial.begin(9600);
 }
 
 void loop() {
     digitalWrite(SWITCH_PIN_A, HIGH);
-    Serial.println(analogRead(MUX_COM_PIN));
-    delay(1);
+    float t = dht.readTemperature();
+    float h = dht.readHumidity();
+    Serial.print("Temp: "); Serial.print(t); Serial.println("°C");
+    Serial.print("Humidity: "); Serial.println(h);
+    delay(500);
 
     digitalWrite(SWITCH_PIN_A, LOW);
     Serial.println(analogRead(MUX_COM_PIN));
-
-    delay(1);
+    delay(500);
 }
