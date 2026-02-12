4fe34c9b40d7548b3a87974a6940b933dd5d7250
marcio <marciogarcial412@gmail.com>
2026-02-12 16:11:14 -0500

    Created simple ETH Test

--- src/ETH_Test.cpp
+++ src/ETH_Test.cpp
@@ -1,22 +1,14 @@
-#include <Arduino.h> //File to test INPUT/OUTPUT of Multiplexer with ETH, Using UART/UDP
-#include <DHT.h>
+#include <Arduino.h>
 
-
-#define DHT_PIN PA0
-#define DHTTYPE DHT11
-
-DHT dht(DHT_PIN, DHTTYPE);
+// Define a hardware serial instance on the specific pins
+HardwareSerial TestSerial(PA10, PA9); // RX, TX (Order depends on core, usually RX, TX)
 
 void setup() {
-    dht.begin();
-    Serial1.begin(9600); // Ready To Send Through ETH
+    // Initialize UART
+    TestSerial.begin(115200);
 }
 
 void loop() {
-    float t = dht.readTemperature();
-    float h = dht.readHumidity();
-    Serial1.print("Temp: "); Serial1.print(t); Serial1.println("°C");
-    Serial1.print("Humidity: "); Serial1.println(h);
-    delay(500);
-
+    TestSerial.println("HELLO!!!");
+    delay(100);
 }
