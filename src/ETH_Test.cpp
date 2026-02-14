f7f71b2244760d094482d826e9cf74d543c375b8
marcio <marciogarcial412@gmail.com>
2026-02-13 19:29:07 -0500

    Updated ETH Test to Include DHT11 Sensor

--- src/ETH_Test.cpp
+++ src/ETH_Test.cpp
@@ -1,14 +1,24 @@
 #include <Arduino.h>
+#include <DHT.h>
+
+#define DHTTYPE DHT11
+#define DHTPIN PD4
+
+DHT dht(DHTPIN, DHTTYPE);
 
 // Define a hardware serial instance on the specific pins
 HardwareSerial TestSerial(PA10, PA9); // RX, TX (Order depends on core, usually RX, TX)
 
 void setup() {
     // Initialize UART
+    dht.begin();
     TestSerial.begin(115200);
 }
 
 void loop() {
-    TestSerial.println("HELLO!!!");
+    float t = dht.readTemperature();
+    float h = dht.readHumidity();
+    TestSerial.print("Temp: "); TestSerial.print(t); TestSerial.println("°C");
+    TestSerial.print("Humidity: "); TestSerial.println(h);
     delay(100);
 }
